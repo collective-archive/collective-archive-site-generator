255591366e276d38bfeb66c510c80d2ee049b292
Chris Geihsler <chris@geihsler.net>
2014-05-11 13:41:22 -0400

    Generating json data from CA.

--- generator.js
+++ generator.js
@@ -1,100 +1,61 @@
-_         = require('underscore');
 fs        = require('fs');
-ejs       = require('ejs');
 path      = require('path')
-sh        = require('exec-sync')
 mkdirp    = require('mkdirp')
 extractor = require('collective-access-extractor')
 
 connectionInfo = {
-  url:      'http://162.243.52.198/',
+  url:      'http://archive.collectivearchivepgh.org/',
   username: 'api',
   password: 'api123'
 };
 
 retrievalOptions = {
-  objects:  [3],
-  entities: [1],
+  objects:  [1, 3],
+  entities: [2, 3, 4],
 };
 
 outputOptions = {
-  target: './dist'
+  target: './src/data'
 };
 
-templates = findTemplates('./templates');
-
-copyAssets(outputOptions);
-
-renderIndex(outputOptions);
+buildIndex(outputOptions);
 
 extractor(connectionInfo, retrievalOptions, function(err, type, thing) {
-  console.log(JSON.stringify(thing));
-  processData(type, thing, outputOptions);
+  buildRecords(type, thing, outputOptions);
 });
 
-function copyAssets(options) {
-  sh("rm -rf " + options.target);
-  sh("cp -rf static " + options.target);
-}
-
-function findTemplates(dir) {
-  files = fs.readdirSync(dir);
-
-  templates = {};
-
-  _.map(files, function(file) {
-    templates[file] = fs.readFileSync(dir + '/' + file, { encoding: 'utf8' });
-  });
-
-  return templates;
-}
-
-function processData(type, data, options) {
+function buildRecords(type, data, options) {
   if(type == 'object') {
-    renderObject(data, options);
+    buildObject(data, options);
   }
   else if(type == 'entity') {
-    renderEntity(data, options);
+    buildEntity(data, options);
   }
 }
 
-function renderIndex(options) {
-  content = renderPartial("_index.html.ejs", {});
-  page    = render(content);
-  filename = options.target + '/index.html', page;
+function buildIndex(options) {
+  json = JSON.stringify({});
+  filename = options.target + '/index.json';
 
   mkdirp.sync(path.dirname(filename));
-  fs.writeFileSync(filename, page);
+  fs.writeFileSync(filename, json);
   console.log('Wrote: ' + filename);
 }
 
-function renderObject(data, options) {
-  content = renderPartial("_object.html.ejs", data);
-  page    = render(content);
-  filename = options.target + '/objects/' + data.id + '.html', page;
+function buildObject(data, options) {
+  json = JSON.stringify(data);
+  filename = options.target + '/objects/' + data.id + '.json';
 
   mkdirp.sync(path.dirname(filename));
-  fs.writeFileSync(filename, page);
+  fs.writeFileSync(filename, json);
   console.log('Wrote: ' + filename);
 }
 
-function renderEntity(data, options) {
-  content = renderPartial("_entity.html.ejs", data);
-  page    = render(content);
-  filename = options.target + '/entities/' + data.id + '.html', page;
+function buildEntity(data, options) {
+  json = JSON.stringify(data);
+  filename = options.target + '/entities/' + data.id + '.json';
 
   mkdirp.sync(path.dirname(filename));
-  fs.writeFileSync(filename, page);
+  fs.writeFileSync(filename, json);
   console.log('Wrote: ' + filename);
-}
-
-function renderPartial(templateName, data) {
-  partial = templates[templateName]
-  data    = _.extend(data, { filename: './templates/' + templateName });
-  return ejs.render(partial, data);
-}
-
-function render(content) {
-  layout = templates["layout.html.ejs"];
-  return ejs.render(layout, { content: content });
 }
