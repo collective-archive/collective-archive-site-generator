a1834427a58bc9ecce68a096aafdf60d1337a12c
Chris Geihsler <chris@geihsler.net>
2014-09-28 10:41:54 -0400

    Showing objects in debug helper.

--- src/helpers.js
+++ src/helpers.js
@@ -13,6 +13,10 @@ module.exports.register = function (Handlebars, options)  {
   Handlebars.registerHelper('debug', function (object)  {
     var html = '';
     _.map(object, function(value, key){
+      if(typeof(value) == "object" || typeof(value) == "array") {
+        value = JSON.stringify(value);
+      }
+
       html += key + ': ' + value + "<br />";
     });
 
