caf0d786865cb0e3583fa3e40721249e9f45d211
Chris Geihsler <chris@geihsler.net>
2014-09-28 09:12:00 -0400

    Hiding carousel buttons if there is only one image.

---
+++ src/scripts/image_carousel.js
@@ -0,0 +1,17 @@
+window.ImageCarousel = function() {
+  return {
+    init: function() {
+      $('.image-carousel').slick({
+        arrows: true,
+        dots: true,
+        prevArrow: '.image-carousel-prev',
+        nextArrow: '.image-carousel-next',
+        onInit: function(slider) {
+          if(slider.slideCount > 1) {
+            $('.image-carousel-button').show();
+          }
+        }
+      });
+    }
+  }
+}();
