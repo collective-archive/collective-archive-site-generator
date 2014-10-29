0889fc6fd859e2aa31db09c098f8a6b14618ba38
Chris Geihsler <chris@geihsler.net>
2014-10-29 13:29:32 -0400

    Added serve to list of default grunt tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -222,5 +222,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('assemble');
   grunt.registerTask('spec',   ['jasmine_node']);
   grunt.registerTask('serve',   ['configureRewriteRules', 'connect:dev', 'watch']);
-  grunt.registerTask('default', ['extract_from_archive', 'extract_from_tumblr', 'prepare_page_data', 'assemble', 'sass', 'copy:dist', 'copy:favicon', 'concat']);
+  grunt.registerTask('default', ['extract_from_archive', 'extract_from_tumblr', 'prepare_page_data', 'assemble', 'sass', 'copy:dist', 'copy:favicon', 'concat', 'serve']);
 }
