703912404662d72187e455b8cb6bf03c397898e4
Chris Geihsler <chris@geihsler.net>
2015-02-28 13:07:14 -0500

    Moved social media stuff into its own module.

---
+++ src/scripts/social.js
@@ -0,0 +1,9 @@
+window.SocialMedia = function() {
+  return {
+    init: function() {
+      $('.social-twitter').data('via', document.URL);
+      $('.social-facebook').attr('href', "http://www.facebook.com/sharer/sharer.php?s=100&amp;p[url]=" + document.URL);
+      $('.social-pinterest').attr('href', "http://pinterest.com/pin/create/button/?url=" + document.URL + "&media=" + $('.fancybox img').attr('src'))
+    }
+  };
+}();
