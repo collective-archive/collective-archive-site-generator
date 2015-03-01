211f2235a1b667ffe76315c81592b545d3cf9d06
Chris Geihsler <chris@geihsler.net>
2015-03-01 04:12:47 -0500

    Rendering search results templates during the build.

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -7,7 +7,11 @@ CA.Search = function() {
         indexUrl: '/search_index.json',
         results : '#search-results',
         entries : '.entries',
-        template: '#search-results-template'
+        render:   function(entries) {
+          return $.map(entries, function(entry) {
+            return entry.html;
+          });
+        }
       });
     }
   };
