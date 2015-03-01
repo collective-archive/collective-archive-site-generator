416139b9af2213872d7eed011bb5910c1639a5f2
Chris Geihsler <chris@geihsler.net>
2015-03-01 13:13:22 -0500

    Fixing carousel issues.

--- src/scripts/image_carousel.js
+++ src/scripts/image_carousel.js
@@ -3,7 +3,7 @@ window.CA = window.CA || {};
 CA.ImageCarousel = function() {
   return {
     init: function() {
-      $('.image-carousel').carousel({
+      $('#image-carousel').carousel({
         interval: false,
         keyboard: true
       });
