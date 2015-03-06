aad9243291529e58af9bb05e92ea50e11fd56b84
Chris Geihsler <chris@geihsler.net>
2015-03-06 16:13:22 -0500

    Using slug links everywhere.

--- src/helpers.js
+++ src/helpers.js
@@ -1,15 +1,4 @@
 module.exports.register = function (Handlebars, options)  {
-  Handlebars.registerHelper('relationshipUrl', function (relationship)  {
-    var plural = function() {
-      if(relationship.type === "object") { return "objects";  }
-      if(relationship.type === "entity") { return "entities"; }
-
-      return "";
-    }
-
-    return new Handlebars.SafeString("/" + plural() + "/" + relationship.id);
-  });
-
   Handlebars.registerHelper('debug', function (object)  {
     var html = '';
     _.map(object, function(value, key){
